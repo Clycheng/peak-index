99da7abc203c3b3ac8c29dfbd2dcab283cf76a1a
Clycheng <30332511+Clycheng@users.noreply.github.com>
2019-01-03 04:44:56 -0600

    Add files via upload

--- 首页/index.js
+++ 首页/index.js
@@ -1,7 +1,25 @@
+// 搜索框的焦点事件
 var Search = document.getElementsByClassName("search")[0];
+
 Search.onfocus = function(){
     this.placeholder = "";   
 }
 Search.onblur = function(){
     this.placeholder = "搜索"; 
+}
+// 选项卡
+var tabLi = document.getElementsByClassName("tab-head")[0].getElementsByTagName("li");
+var tabCont = document.getElementsByClassName("selected")[0].getElementsByTagName("div");
+for (let i = 0; i <tabLi.length; i ++){
+    tabLi[i].onclick =function(){
+        for( let z = 0; z <tabLi.length; z ++){
+            for( let h = 0; h<tabLi.length; h ++){
+                tabLi[h].classList.remove("active");
+                tabCont[h].classList.remove("select");
+            }
+         
+        }
+        this.classList.add("active");
+        tabCont[i].classList.add("select");
+    }
 }
